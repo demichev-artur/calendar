4bd88c8af11a8d2c61c38f9ae735f1102d44fb35
demichev-artur <artur.demichev03@mail.ru>
2022-07-27 09:56:53 +0600

    feat: added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import LogIn from "./pages/LogIn/LogIn";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LogIn/>}/>
+            </Routes>
+        </BrowserRouter>
+
     );
 }
 
